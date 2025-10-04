562ce5e87760e9240a0a84750c35e9f40d88bfda
Bart Veneman <bartveneman@gmail.com>
2025-10-04 11:24:04 +0200

    remove unused/commented playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -58,52 +58,7 @@ const config: PlaywrightTestConfig = {
 				userAgent: devices['Desktop Chrome'].userAgent + ' (Playwright Test)',
 			}
 		}
-
-		// {
-		//   name: 'firefox',
-		//   use: {
-		//     ...devices['Desktop Firefox'],
-		//   },
-		// },
-
-		// {
-		//   name: 'webkit',
-		//   use: {
-		//     ...devices['Desktop Safari'],
-		//   },
-		// },
-
-		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: {
-		//     ...devices['Pixel 5'],
-		//   },
-		// },
-		// {
-		//   name: 'Mobile Safari',
-		//   use: {
-		//     ...devices['iPhone 12'],
-		//   },
-		// },
-
-		/* Test against branded browsers. */
-		// {
-		//   name: 'Microsoft Edge',
-		//   use: {
-		//     channel: 'msedge',
-		//   },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: {
-		//     channel: 'chrome',
-		//   },
-		// },
 	],
-
-	/* Folder for test artifacts such as screenshots, videos, traces, etc. */
-	// outputDir: 'test-results/',
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
