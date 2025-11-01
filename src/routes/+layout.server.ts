dd654d9954547ed320f5bab79a19d330e40ddccf
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2025-11-01 11:34:53 +0100

    fix: prevent running analytics on preview deploys (log-only) (#45)

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,31 +1,28 @@
 export const trailingSlash = 'never' // or 'always' or 'ignore', depending on what you want
 
 import { dev } from '$app/environment'
+import { env } from '$env/dynamic/private'
 
 function should_allow_analytics({
 	dev,
-	user_agent,
+	deploy_context,
+	user_agent
 }: {
 	dev: boolean
+	deploy_context?: string
 	user_agent: string | null
 }): boolean {
-	if (dev)
-		return false
+	if (dev) return false
+
+	if (deploy_context !== 'production') return false
 
 	if (user_agent) {
 		user_agent = user_agent.toLowerCase()
 
-		if (user_agent.includes('playwright'))
-			return false
-
-		if (user_agent.includes('sentry'))
-			return false
-
-		if (user_agent.includes('bot'))
-			return false
-
-		if (user_agent.includes('headless'))
-			return false
+		if (user_agent.includes('playwright')) return false
+		if (user_agent.includes('sentry')) return false
+		if (user_agent.includes('bot')) return false
+		if (user_agent.includes('headless')) return false
 	}
 
 	return true
@@ -34,7 +31,8 @@ function should_allow_analytics({
 export function load({ locals, request }) {
 	let allow_analytics = should_allow_analytics({
 		dev,
-		user_agent: request.headers.get('user-agent'),
+		deploy_context: env.CONTEXT,
+		user_agent: request.headers.get('user-agent')
 	})
 
 	return {
