e2ff36d89df5f9f83776d1e4595220ad15a89679
Bart Veneman <bartveneman@gmail.com>
2025-12-23 20:34:28 +0100

    more fixes

--- src/lib/components/custom-property-inspector/analyze-custom-properties.ts
+++ src/lib/components/custom-property-inspector/analyze-custom-properties.ts
@@ -18,43 +18,27 @@ export function analyze(css: string) {
 	let all_properties = new Map<string, CssLocation[]>()
 	let declared_with_fallback = new Set<string>()
 
-	// Helper to recursively walk values and find all var() functions
-	function walk_values(value_node: CSSNode, declaration_node: CSSNode) {
-		if (value_node.type === FUNCTION && value_node.name === 'var') {
-			let first_child = value_node.first_child
-			if (first_child !== null && first_child.type === IDENTIFIER && first_child.name.startsWith('--')) {
-				let loc = to_loc(declaration_node)
+	walk(ast, (node: CSSNode) => {
+		if (node.type === DECLARATION) {
+			if (node.property.startsWith('--')) {
+				let name = node.property
+				let loc = to_loc(node)
+				declared_properties.add(name)
+				all_properties.set(name, (all_properties.get(name) ?? []).concat(loc))
+			}
+		} else if (node.type === FUNCTION && node.name === 'var') {
+			let [first_child, second_child] = node.children
+
+			if (first_child?.type === IDENTIFIER && first_child.name.startsWith('--')) {
+				let loc = to_loc(node)
 				let name = first_child.name
 				used_properties.add(name)
 				all_properties.set(name, (all_properties.get(name) ?? []).concat(loc))
 
 				// check if it has a fallback value that is a custom property
-				let children = value_node.children
-				let second_child = children[1]
 				if (second_child !== undefined && !(second_child.type === FUNCTION && second_child.name === 'var')) {
 					declared_with_fallback.add(name)
 				}
-			}
-		}
-
-		// Recursively walk children to find nested var() calls
-		for (let child of value_node.children) {
-			walk_values(child, declaration_node)
-		}
-	}
-
-	walk(ast, (node: CSSNode) => {
-		if (node.type === DECLARATION) {
-			if (node.property.startsWith('--')) {
-				let loc = to_loc(node)
-				let name = node.property
-				declared_properties.add(name)
-				all_properties.set(name, (all_properties.get(name) ?? []).concat(loc))
-			}
-
-			// Check for var() usage in declaration values (recursively)
-			for (let value of node.values) {
-				walk_values(value, node)
 			}
 		} else if (node.type === AT_RULE && node.name === 'property') {
 			let name = node.prelude!
