ebca8dd778a893b65301671129eac3a48d6a05df
Bart Veneman <bartveneman@gmail.com>
2026-09-09 17:09:11 +0200

    add page to navigations etc.

--- src/lib/components/Nav.items.ts
+++ src/lib/components/Nav.items.ts
@@ -4,6 +4,7 @@ export const items = [
 	{ url: '/design-tokens', title: 'Design Tokens' },
 	{ url: '/css-code-quality', title: 'Code Quality' },
 	{ url: '/custom-property-inspector', title: 'Custom properties' },
+	{ url: '/baseline-overview', title: 'Baseline overview' },
 	{ url: '/css-coverage', title: 'Code coverage' },
 	{ url: '/the-css-selection/2026', title: 'The CSS Selection' }
 ]
